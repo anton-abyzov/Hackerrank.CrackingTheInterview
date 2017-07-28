0af8784936dac9ae9d1578f35a5e40bffcb8f063
Anton <anton.abyzov@gmail.com>
2017-07-28 04:35:52 +0300

    counting inversions first tests pass

---
+++ CountingInversions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        int t = Convert.ToInt32(Console.ReadLine());
+        for (int a0 = 0; a0 < t; a0++)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] arr_temp = Console.ReadLine().Split(' ');
+            _temp = new int[n];
+            int[] arr = Array.ConvertAll(arr_temp, Int32.Parse);
+            CountInversions(arr, 0, arr.Length - 1);
+
+            Console.WriteLine(_count);
+        }
+    }
+
+    private static int[] _temp;
+    private static long _count;
+
+    private static void CountInversions(int[] arr, int start, int end)
+    {
+        if (start >= end)
+            return;
+
+        var mid = (start + end) / 2;
+        CountInversions(arr, 0, mid);
+        CountInversions(arr, mid + 1, end);
+        Merge(arr, start, end);
+    }
+
+    private static void Merge(int[] arr, int start, int end)
+    {
+        var mid = (start + end) / 2;
+        var leftIndex = start;
+        var rightIndex = mid + 1;
+        var tempIndex = start;
+        while (leftIndex <= mid && rightIndex <= end)
+        {
+            if (arr[leftIndex] <= arr[rightIndex])
+            {
+                _temp[tempIndex++] = arr[leftIndex++];
+            }
+            else
+            {
+                _temp[tempIndex++] = arr[rightIndex++];
+                _count += mid - leftIndex + 1;
+            }
+        }
+
+        Array.Copy(arr, leftIndex, _temp, tempIndex, mid - leftIndex + 1); // +1 coz it's actually cound of elements
+        Array.Copy(arr, rightIndex, _temp, tempIndex, end - rightIndex + 1);
+        Array.Copy(_temp, start, arr, start, end - start + 1);
+    }
+}
